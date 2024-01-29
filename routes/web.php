3daf0c44ffb8c10d49642ce6cab9992d9d0060a3
hamza <hamzakhalem97@gmail.com>
2024-01-29 18:32:33 +0100

    auth flow

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,15 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/home', [HomeController::class,'index'])->name('home');
+Route::get('/', [HomeController::class,'index'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
